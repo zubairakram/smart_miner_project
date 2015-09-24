72f2b9652a7c199c4aebf4211d45d1f8b2dc130b
Zubair Akram <zubair616@gmail.com>
2015-09-24 17:14:18 +0500

    some changes made

--- smart_miner/urls.py
+++ smart_miner/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
-from smart_miner.views import index
+
+from smart_miner import views
+
 
 urlpatterns = [
-    url(r'$', index),
+    url(r'login/$', views.login, name='login'),
+    url(r'hello/$', views.hello , name='hello'),
 ]
--- smart_miner/views.py
+++ smart_miner/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
-def index(request):
-    return render(request, 'index.html')
+
+
+def login(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render(request, 'login.html',{'state':state, 'username': username})
+
+
+def hello(request):
+    
+    return render(request, 'hello.html', {"name": "Zubair"})
